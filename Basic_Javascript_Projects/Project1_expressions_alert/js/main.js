48b5b7461e7fe251106c7868ce2cd33feb5f1bf6
rebutta <101282383+rebutta@users.noreply.github.com>
2022-03-27 01:42:08 -0700

    Basic JavaScript Projects Submission

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,22 @@
+alert('Hello, World!'); //Alert when webpage opens
+
+var A = "String\'s are the best kind of variables! " //assinged variable to A
++ "If given the opportunity, I will show you why " //string1 for var A
++ "string\'s are the best variable." //string2 for var A
+
+document.write(A) //variable A called on
+
+var Z = Dog = "Marty", Cat = "Betty", Hamster = "Jerry", Goldfish = "Pistachio"; //assinged variables to var Z
+
+document.write(Goldfish); //variable Zgoldfish called on
+
+function My_First_Function() { //assigning data to function
+    var str = "This is the button text!"; //variable assigned to My_First_Function
+    document.getElementById("Button_Text").innerHTML = str; //button is assigned to My_First_Function
+}
+
+var W = 8
+
+var M = 8
+
+document.write(W+M)
